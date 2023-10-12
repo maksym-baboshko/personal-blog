ecb68dd80abcae19d2b9c844d14288957b5c53e0
Maksym Baboshko <maksym.baboshko@outlook.com>
2023-10-12 12:24:12 +0200

    Remove deprecated @storybook/addon-styling from the storybook config

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,7 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
 
-import { buildCssLoader } from '../build/lib'
-
 const config: StorybookConfig = {
   framework: { name: '@storybook/react-webpack5', options: {} },
   stories: ['../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -9,8 +7,7 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    { name: '@storybook/addon-styling', options: { cssBuildRule: buildCssLoader(true) } }
+    '@storybook/addon-interactions'
   ],
   docs: { autodocs: 'tag', defaultName: 'Brief' }
 }
